ed2ba33c11182c18fffb9642e5207f50d98c7911
Senyaa <kolejnik12@gmail.com>
2024-02-15 14:22:07 +0100

    test

--- drizzle/drizzle.ts
+++ drizzle/drizzle.ts
@@ -1,11 +1,5 @@
 import * as schema from "./drizzleSchema";
 import { drizzleVercel, drizzleVercelConnection } from "./drizzleVercel";
-import { drizzleLocal, drizzleLocalConnection } from "./drizzleLocal";
 
-const isVercel = process.env.PRISMA_SCHEMA_DISABLE_ADVISORY_LOCK;
-
-export const dbConnection = isVercel
-  ? drizzleVercelConnection
-  : drizzleLocalConnection;
-export const drizzle = isVercel ? drizzleVercel : drizzleLocal;
+export const drizzle = drizzleVercel;
 export const drizzleSchema = schema;
