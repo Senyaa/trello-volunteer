c3e7f51edb1635b68cd08925edc518dcc7e044d7
Senyaa <kolejnik12@gmail.com>
2024-02-19 13:21:42 +0100

    fix postgres connection

--- drizzle/drizzleLocal.ts
+++ drizzle/drizzleLocal.ts
@@ -1,7 +1,18 @@
 import * as schema from "./drizzleSchema";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-export const drizzleLocalConnection = postgres(process.env.DATABASE_URL || "");
+declare global {
+  // eslint-disable-next-line no-var -- only var works here
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
 
-export const drizzleLocal = drizzle(drizzleLocalConnection, { schema });
+let drizzleLocal: PostgresJsDatabase<typeof schema>;
+
+if (!global.db) {
+  global.db = drizzle(postgres(process.env.DATABASE_URL || ""), { schema });
+}
+
+drizzleLocal = global.db;
+
+export { drizzleLocal };
