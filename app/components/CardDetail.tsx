e8f4106f33fa69bc08ea372f925dba76eae2935d
Senyaa <kolejnik12@gmail.com>
2024-02-22 19:19:03 +0100

    wrap card detail

--- app/components/CardDetail.tsx
+++ app/components/CardDetail.tsx
@@ -18,7 +18,7 @@ const CardDetail: FC<DetailProps> = ({
   return (
     <div className={isOn ? width : "hidden"}>
       {visible && (
-        <div className="whitespace-pre-wrap mt-2 md:mt-0 flex gap-2 md:gap-0 w-full rounded-md bg-neutral-100 dark:bg-neutral-800 border-neutral-200 dark:border-black border p-2">
+        <div className="text-wrap mt-2 md:mt-0 flex gap-2 md:gap-0 w-full rounded-md bg-neutral-100 dark:bg-neutral-800 border-neutral-200 dark:border-black border p-2">
           <div className="md:hidden">{icon}</div>
           <span>{text}</span>
         </div>
