88623fa1959cf942eae7e697ffcfd4f1c0cde13d
Senyaa <kolejnik12@gmail.com>
2024-03-24 20:38:03 +0100

    add 3rd shift link

--- app/(site)/protected/home/links.ts
+++ app/(site)/protected/home/links.ts
@@ -7,6 +7,10 @@ export const links = [
     link: process.env.NEXT_PUBLIC_CATS_AND_DOGS_SHEET_2,
     label: "Grafik psy i koty (11.03 - 24.03)",
   },
+  {
+    link: process.env.NEXT_PUBLIC_CATS_AND_DOGS_SHEET_3,
+    label: "Grafik psy i koty (25.03 - 07.04)",
+  },
   { link: process.env.NEXT_PUBLIC_WILDS_SHEET, label: "Grafik dzikie" },
   { link: process.env.NEXT_PUBLIC_FB_GROUP, label: "Grupa FB" },
   { link: process.env.NEXT_PUBLIC_ACTIVITY_SHEET, label: "Plik aktywności" },
