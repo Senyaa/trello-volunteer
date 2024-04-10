23ed44466691b4de9a852981917fb54a486bd56e
Senyaa <kolejnik12@gmail.com>
2024-04-10 16:21:47 +0200

    new cats and dogs sheet (08.04.24)

--- app/(site)/protected/home/links.ts
+++ app/(site)/protected/home/links.ts
@@ -1,16 +1,17 @@
 export const links = [
   {
     link: process.env.NEXT_PUBLIC_CATS_AND_DOGS_SHEET_1,
-    label: "Grafik psy i koty (26.02 - 10.03)",
-  },
-  {
-    link: process.env.NEXT_PUBLIC_CATS_AND_DOGS_SHEET_2,
-    label: "Grafik psy i koty (11.03 - 24.03)",
+    label: "Aktualny grafik psy i koty (08.04 - 21.04)",
   },
   {
     link: process.env.NEXT_PUBLIC_CATS_AND_DOGS_SHEET_3,
     label: "Grafik psy i koty (25.03 - 07.04)",
   },
+  {
+    link: process.env.NEXT_PUBLIC_CATS_AND_DOGS_SHEET_2,
+    label: "Grafik psy i koty (11.03 - 24.03)",
+  },
+
   { link: process.env.NEXT_PUBLIC_WILDS_SHEET, label: "Grafik dzikie" },
   { link: process.env.NEXT_PUBLIC_FB_GROUP, label: "Grupa FB" },
   { link: process.env.NEXT_PUBLIC_ACTIVITY_SHEET, label: "Plik aktywności" },
